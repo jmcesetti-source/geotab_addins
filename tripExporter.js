81020737ca4b9524c7264040ce56b954cf9c39a7
jmcesetti-source <jmcesetti@gmail.com>
2025-11-10 10:49:55 -0300

    Update tripExporter.js

--- tripExporter.js
+++ tripExporter.js
@@ -1,17 +1,18 @@
-geotab.addin.tripExporter = function (api, state) {
-    return {
-        initialize: function (api, state, callback) {
-            callback();
-        },
-        focus: function (api, state) {
-            const button = document.getElementById("addinButton_tripExporter");
-            if (button) {
-                button.onclick = function () {
-                    // Abre una nueva pestaña
-                    window.open("https://www.google.com", "_blank");
-                };
-            }
-        },
-        blur: function () {}
-    };
+/**
+ * Opens a new Chrome tab when the button is clicked.
+ *
+ * @param {object} event - The event dispatched from the button click.
+ * @param {object} api - The GeotabApi object.
+ * @param {object} state - The page state object.
+ */
+geotab.customButtons.tripExporter = (event, api, state) => {
+  'use strict';
+
+  event.preventDefault();
+
+  // Example: open Google in a new tab
+  const newTabUrl = 'https://www.google.com';
+
+  // Open a new browser tab
+  window.open(newTabUrl, '_blank');
 };
